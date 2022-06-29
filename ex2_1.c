2d84550e99a4dc84d06fabf55af16db355169f1c
ssozzang <108378941+ssozzang@users.noreply.github.com>
2022-06-29 21:24:34 +0900

    Create ex2_1.c
    x+y=45
    z=40
    계속하려면 아무 키나 누르십시오...

---
+++ ex2_1.c
@@ -0,0 +1,17 @@
+#include <>stdio.h>
+
+void main()
+{
+	int x,y,z;
+	
+	x = 10;
+	y = 35;   //printf("z=%d \n", z)
+	
+	z = x+ y;
+	printf("x+y=%d \n", z);
+	
+	z = 40;
+	printf("z = %d \n, z");
+	
+	
+}
