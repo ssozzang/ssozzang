79c3bb4b3a23f3aa7987eb68228ee7e23b370da5
ssozzang <108378941+ssozzang@users.noreply.github.com>
2022-06-29 21:30:31 +0900

    Create e2_3.c
    a = A  b = a  c =0
    a = 65, b = 97, c = 48
    a = 41, b = 61, c = 30
    계속하려면 아무 키나 누르십시오.....

---
+++ e2_3.c
@@ -0,0 +1,11 @@
+#include <>stdio.h>
+
+void main()
+{
+	char a ='A', b ='a', c = '0';
+	
+	printf("a = %c, b =%c, c = %c \n", a, b, c);
+	printf("a = %d, b =%d, c = %d \n", a, b, c);
+	printf("a = %x, b =%x, c = %x \n", a, b, c);
+	
+}
