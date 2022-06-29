14c00067d1627921bb11cfd65c11b8c27a37f33f
ssozzang <108378941+ssozzang@users.noreply.github.com>
2022-06-29 21:18:09 +0900

    ex1_3.c

---
+++ ex1_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+	int seore1= 80, score2= 70, score3= 90;
+	int sum;
+	
+	sum=0;
+	sum= sum + score1;
+	sum= sum + score2;
+	sum= sum + score3;
+	
+	printf("score2, score2, score3의 합은 %d이다.\n", sum);
+}
