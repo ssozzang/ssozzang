a9842f9bcc952b1bdd6e8a7e6dd70fc833a61270
ssozzang <108378941+ssozzang@users.noreply.github.com>
2022-06-29 21:38:57 +0900

    Create ex2_7.c
    str1: Hello
    str2: Good
    str3: Korea
    str3의 길이 : 6

---
+++ ex2_7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+     char str1[6] = "Hello";
+     char str2[5] = {'G', 'o', 'o', 'd', '\o';
+     char str3[] = "Korea";
+                     
+     printf("str1 : %s \n", str1);
+     printf("str2 : %s \n", str2); 
+     printf("str3 : %s \n", str3);
+     printf("str3의 길이 : %ㅇ \n", sizeof(str3));  
+ 
+   }
