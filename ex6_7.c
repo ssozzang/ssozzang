6a4f6148ad11c472173bcaca251ba607f394fe74
ssozzang <108378941+ssozzang@users.noreply.github.com>
2022-06-30 20:49:36 +0900

    Create ex6_7.c
    1부터 100까지 정수의 합은 5050

---
+++ ex6_7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+      int = 0, sum =0;
+      
+      while (i < 100){
+          i ++;
+          sum += i;
+      }
+      printf("1부터 100까지 정수의 합은 %d \n", sum);
+}
